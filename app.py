a32840d0afca4363dffc0cd9fb8badc03b809ccf
sdfsdfdsf <6yonor@gmail.com>
2018-03-01 15:05:59 +0200

    s

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config["DEBUG"] = True
 @app.route("/hello")
 # define the view using a function, which returns a string
 def hello_world():
-    return "Hello, World!"
+    return "Hello, World!!!!!!"
 # dynamic route
 @app.route("/test/<search_query>")
 def search(search_query):
@@ -22,6 +22,25 @@ def index(name):
         return "Hello, {}".format(name)
     else :
         return "Not Found", 404
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return "correct"
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value + 1)
+    return "correct"
+#@app.route("/name/<name>")
+#def index(name):
+#    if name.lower() == "michael":
+#        return "Hello, {}".format(name)
+#    else:
+#        return "Not Found", 404
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run()
